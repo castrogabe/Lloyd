ae3e14f65376b976004933ae8a261be01a9c79f6
Gabriel Castro <43317756+castrogabe@users.noreply.github.com>
2025-04-24 06:21:22 -0700

    11th Commit-ProductMag Lightbox, Options, nodemailer purchase receipt

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,22 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-module.exports = {
+export default {
   PORT: process.env.PORT || 8000,
-  JWT_SECRET: process.env.JWT_SECRET || 'Objetsdart',
+  JWT_SECRET: process.env.JWT_SECRET || 'collecting',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   MONGODB_URL: process.env.MONGODB_URI || 'mongodb://localhost/frontend',
 
-  auth: process.env.NODE_USER,
-  auth: process.env.NODE_PASSWORD,
+  CLOUDINARY_CLOUD_NAME: 'dqwurwyuf',
+  CLOUDINARY_API_KEY: process.env.key || 'apiKey',
+  CLOUDINARY_URL: process.env.cloudinary || 'secretKey',
+  CLOUDINARY_API_SECRET: process.envCloudinary || 'secretAccessKey',
+
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || 'your_stripe_secret_key',
+  STRIPE_PUBLISHABLE_KEY:
+    process.env.STRIPE_PUBLISHABLE_KEY || 'your_stripe_publishable_key',
+
+  auth: process.env.NODE_USER || 'username',
+  auth: process.env.NODE_PASSWORD || 'password',
 };
